e90a5723132a20aa8d882b47a0990f06c6826454
郭爽 <2997663084@qq.com>
2019-08-07 09:12:17 +0800

    112

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,8 +5,5 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("ÐÞ¸ÄHelloWorld!");
-        System.out.println("2ÐÞ¸ÄHelloWorld!");
-		
     }
 }
