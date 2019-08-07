d3f2eb21b8de7dacdc6fcfb22e8107c6a8f417b4
郭爽 <2997663084@qq.com>
2019-08-07 09:42:38 +0800

    老员工

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("新代码");
-		System.out.println("新新员工提交代码");
+		System.out.println("新员工提交代码");
 		System.out.println("老员工员工提交代码");
+		System.out.println("老员工员工再次提交代码");
+
     }
 }
